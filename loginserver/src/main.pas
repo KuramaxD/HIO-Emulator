1e0612ce605233ae1f4c3bcac626af00d251fe3f
KuramaxD <sonexa@gmail.com>
2014-03-04 07:32:10 -0300

    Adicinado comentário a unit main

--- loginserver/src/main.pas
+++ loginserver/src/main.pas
@@ -1,5 +1,20 @@
 unit main;
 
+(*
+
+Unit que contém a função iniciar(); Que serve para ligar o servidor.
+
+Organizado por:
+Felipe de Souza Camargo(Kurama)
+
+Sobre o funcionamento do código:
+Apenas chame a função iniciar(); Ao abrir o programa.
+
+Referências:
+Sem referências no momento
+
+*)
+
 interface
 
 uses Windows, SysUtils, colors, funcoes, crypts, packetprocess, database, sockets;
@@ -10,14 +25,8 @@ implementation
 
 procedure iniciar();
 begin
-  //======================================
-  //  PREPARA O SERVIDOR PARA SER LIGADO
-  //======================================
-  SetConsoleTitle('PY Login GB.R6.726.00');
+  SetConsoleTitle('HIO-Emulator Login Server');
   Writeln('[SERVER_S] Iniciando servidor.');
-  //======================================
-  //  ARRUMA AS PROCEDURES DO COMPONENTE
-  //======================================
   iniciardatabase('127.0.0.1','root','vertrigo','pangya',3306);
   iniciarsocket(10103);
 end;
